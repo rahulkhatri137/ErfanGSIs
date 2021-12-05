5a9a53161af7866e43b381c02a95d2124dad8458
Rahul <82636553+rahulkhatri137@users.noreply.github.com>
2021-12-05 12:01:46 +0530

    Update mkimage.sh

--- scripts/mkimage.sh
+++ scripts/mkimage.sh
@@ -82,6 +82,7 @@ if [[ -f "$tempdir/file_contexts" ]]; then
     echo "/logcat                 u:object_r:rootfs:s0" >> "$tempdir/file_contexts"
     echo "/preload                u:object_r:rootfs:s0" >> "$tempdir/file_contexts"
     echo "/elabel                 u:object_r:rootfs:s0" >> "$tempdir/file_contexts"
+    echo "/oppo_product           u:object_r:rootfs:s0" >> "$tempdir/file_contexts"
     if [ ! "$build" == false ]; then
         if [ -f "$build/file_contexts" ]; then
             cat "$build/file_contexts" >> "$tempdir/file_contexts"
